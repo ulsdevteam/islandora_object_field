2600254b0b13899ef93af2cdab5e1e17cf852fe1
Clinton Graham <ctgraham@pitt.edu>
2023-02-01 12:01:45 -0500

    cwrc#6: Enable Solr search formatter

--- templates/islandora-object-field-label.tpl.php
+++ templates/islandora-object-field-label.tpl.php
@@ -6,6 +6,7 @@
  *
  * Available variables:
  * - $label: The label text.
+ * - $object: The Fedora object
  * - $pid: The ID of the object whose label is being displayed.
  */
 echo l($label, 'islandora/object/' . $pid); ?>
